49b2a5037d5b3ce535f812a3b4b1ea911e43fb91
jungjunhyung99 <humor12@nate.com>
2022-01-24 22:35:01 +0900

    내용 지움

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <h1>지웠엉</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
